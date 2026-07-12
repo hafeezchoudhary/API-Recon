57dee332885e02e41f2857ab3af56724a9d57846
Hafeez choudhary <choudharyhafeez2513@gmail.com>
2026-07-12 18:34:32 +0530

    Implement JSON file selection

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,28 @@
-from flask import Flask
+from flask import Flask, request, flash, jsonify
 from flask_cors import CORS
+import requests
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/")
-def home():
+@app.route("/") 
+def home() :
+
     return {
         "status": "running",
         "message": "APILens Backend is running successfully!"
     }
+
+@app.route("/upload", methods=["GET", "POST"])
+def upload() :
+    if "file" not in request.files:
+        return jsonify({"error": "No file part in the request"}), 400      
+
+    uploaded_file = request.files["file"] 
+    if uploaded_file :
+        return {
+            "message": f"{uploaded_file.name}"
+        }  
 
 if __name__ == "__main__":
     app.run(debug=True)

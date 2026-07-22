a846077f5638ed56cafd2787b703ee6ce2f15884
Hafeez choudhary <choudharyhafeez2513@gmail.com>
2026-07-22 17:25:22 +0530

    Add responsive dashboard with dynamic API analysis cards and endpoint table

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,14 +6,64 @@ import Home from './pages/Home'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import './App.css'
+import Hero from './components/Hero'
+import Dashboard from './components/Dashboard'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [analysis, setAnalysis] = useState(null);
+  const [selectedFile, setSelectedFile] = useState("")
+  const [loading, setLoading] = useState(false);
+
+  const handleUpload = async (file) => {
+    if (!file) {
+      return alert("Select a file first")
+    }
+    const formData = new FormData()
+    formData.append("file", file)
+
+    try {
+      setLoading(true)
+      const response = await fetch("http://localhost:5000/upload", {
+        method: "POST",
+        body: formData
+      })
+
+      const data = await response.json()
+      setAnalysis(data)
+      setLoading(false)
+
+    } catch (e) {
+      console.error('Error uploading file:', e)
+      console.log('An error occurred during upload.')
+    }
+
+  }
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0]
+    setSelectedFile(file)
+    handleUpload(file)
+  }
+
+  const downloadreport = () => {
+    window.open("http://localhost:5000/download-report")
+  }
 
   return (
     <>
       <Navbar></Navbar> 
-      <Home></Home>
+      {/* <Home></Home> */}
+      <Hero
+        analysis={analysis}
+        loading={loading}
+        selectedFile={selectedFile}
+        handleFileChange={handleFileChange}
+      />
+
+      {analysis && (
+        <Dashboard analysis={analysis} />
+      )}
       <Footer></Footer>
     </>
   )
